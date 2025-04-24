8a55f6699ae906db166588c6b5ebb0f6aa5d8ce8
1chary <abhilashchary04@gmail.com>
2025-04-24 22:25:31 +0530

    added button

--- myApp/src/components/Product/index.jsx
+++ myApp/src/components/Product/index.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Header from '../Header'
 
 const Product = () => {
+
+  const consoleMessage = () => {
+    console.log("hello this is the testing")
+  }
+
   return (
     <div>
         <Header/>
         <h1>This is the Product page i have changed just now</h1>
         <span>To test weather the merge is working fine or not</span>
         <span>This is the another line to check merge is working or not</span>
+        <h1>Ram siya</h1>
+        <button onClick={consoleMessage}>hi welcome to the another testing phase</button>
     </div>
   )
 }
