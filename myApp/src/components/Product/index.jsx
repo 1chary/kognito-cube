6e150e20f9abaf9a9126b7390da79297103561a6
1chary <abhilashchary04@gmail.com>
2025-04-24 22:12:09 +0530

    testing

--- myApp/src/components/Product/index.jsx
+++ myApp/src/components/Product/index.jsx
@@ -5,7 +5,9 @@ const Product = () => {
   return (
     <div>
         <Header/>
-        <h1>Product page</h1>
+        <h1>This is the Product page i have changed just now</h1>
+        <span>To test weather the merge is working fine or not</span>
+        <span>This is the another line to check merge is working or not</span>
     </div>
   )
 }
